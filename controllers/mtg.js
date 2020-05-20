0730be3f5b8b7169efe93d0b37ca9963ce952365
Pekka Tuukkanen <petuukka@gmail.com>
2020-05-20 11:31:12 +0300

    add test get route

--- controllers/mtg.js
+++ controllers/mtg.js
@@ -2,6 +2,11 @@ const mtgRouter = require('express').Router()
 
 mtgRouter.post('/', async (request, response) => {
 	console.log(request.body)
+	response.send('jepa')
+})
+
+mtgRouter.get('/', async (request, response) => {
+	response.send('jejeje')
 })
 
 module.exports = mtgRouter
--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = require('./app')
 
 const server = http.createServer(app)
 
-server.listen(3001, () => {
+server.listen(3001, '0.0.0.0', () => {
 	console.log(`Server running on port 3001`)
 })
